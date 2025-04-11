c990e2ccf6b10f24d1ea1dc92ee762de0aaa453b
simon911252 <simon911252@gmail.com>
2025-04-11 14:36:55 -0400

    Collisions integrated

--- projet_final/babyfoot_table_vpython.py
+++ projet_final/babyfoot_table_vpython.py
@@ -1,6 +1,6 @@
 from vpython import canvas, box, sphere, vector, color, rate, mag
 import numpy as np
-import math
+import time
 
 #toutes les mesures sont en mm (x, y, z)
 
@@ -56,19 +56,14 @@ for i, rod_pos in enumerate(red_rod_positions):
 
 # Create the ball at the specified position
 ball = sphere(pos=vector(ball_initial_pos[0], ball_initial_pos[1], 0.15), radius=ball_radius, color=color.white)
-ball_initial_dir = [element / math.sqrt(ball_initial_dir[0]**2 + ball_initial_dir[1]**2) for element in ball_initial_dir]
+ball_initial_dir = [element / np.sqrt(ball_initial_dir[0]**2 + ball_initial_dir[1]**2) for element in ball_initial_dir]
 ball_velocity = vector(ball_initial_dir[0]*ball_initial_velocity, ball_initial_dir[1]*ball_initial_velocity, 0)
 
 
 # Create the nets behind each goalkeeper
 net_blue = box(pos=vector(-table_length/2-net_depth/2, 0, 0.15), size=vector(net_depth, net_height, net_thickness), color=color.white)
 net_red = box(pos=vector(table_length/2+net_depth/2, 0, 0.15), size=vector(net_depth, net_height, net_thickness), color=color.white)
-
-
-def move_rod(color : str, rod_number : int, mov_amount : int):
-    #for rod number, gk = 0, def = 1, mid = 2, att = 3
-    #mov_amount in mm
-    pass
+nets = [net_blue, net_red]
 
 
 
@@ -76,6 +71,7 @@ maximal_dist_of_collision = ball_radius + np.sqrt(pwn_sz[0]**2 + pwn_sz[1]**2) #
 
 print("Press 'q' to quit the simulation.")
 
+most_recent_pawn = None
 while True:
     rate(300)
     # Move the ball
@@ -84,19 +80,32 @@ while True:
     # Check for collisions with table boundaries
     if abs(ball.pos.x) > table_length/2:
         ball_velocity.x *= -1
+        most_recent_pawn = None
     if abs(ball.pos.y) > table_width/2:
         ball_velocity.y *= -1
+        most_recent_pawn = None
+
+    # for pwn in pwns:
+    #     if abs(ball.pos.x - pwn.pos.x) < (ball.radius + pwn.size.x / 2) and abs(ball.pos.y - pwn.pos.y) < (ball.radius + pwn.size.y / 2):
+    #         ball_velocity.x *= -(np.random.uniform(0.2, 2))
+    #         if abs(ball_velocity.x) > ball_max_velocity:
+    #             ball_velocity.x = ball_max_velocity*math.copysign(1, ball_velocity.x)
+    #         elif abs(ball_velocity.x) < ball_min_velocity:
+    #             ball_velocity.x = ball_min_velocity*math.copysign(1, ball_velocity.x)
 
     # Check for collisions with players
+    
     for pwn in pwns:
-        if mag(pwn.pos - ball.pos) < maximal_dist_of_collision:
-            print("collision")
-        break
-        
-        
-        if abs(ball.pos.x - player.pos.x) < (ball.radius + player.size.x / 2) and abs(ball.pos.y - player.pos.y) < (ball.radius + player.size.y / 2):
-            ball_velocity.x *= -(np.random.uniform(0.2, 2))
-            if abs(ball_velocity.x) > ball_max_velocity:
-                ball_velocity.x = ball_max_velocity*math.copysign(1, ball_velocity.x)
-            elif abs(ball_velocity.x) < ball_min_velocity:
-                ball_velocity.x = ball_min_velocity*math.copysign(1, ball_velocity.x)
+        pawn_to_ball_vct = pwn.pos - ball.pos
+        pawn_to_ball_distance = mag(pawn_to_ball_vct)
+        if pawn_to_ball_distance < maximal_dist_of_collision and most_recent_pawn != pwn:
+            if abs(ball.pos.x - pwn.pos.x) < (ball.radius + pwn.size.x / 2) and abs(ball.pos.y - pwn.pos.y) < (ball.radius + pwn.size.y / 2):
+                x_pos, y_pos = -pawn_to_ball_vct.x, -pawn_to_ball_vct.y
+                angle = np.rad2deg(np.arctan2(y_pos, x_pos))
+                ball_velocity.x *= -1
+                most_recent_pawn = pwn
+            break
+    
+    for net in nets:
+        if abs(ball.pos.x - net.pos.x) < (ball.radius + net.size.x / 2) and abs(ball.pos.y - net.pos.y) < (ball.radius + net.size.y / 2):
+            print("GOAL")
