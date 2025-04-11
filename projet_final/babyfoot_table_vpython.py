871819b5494014431647cf29eaad6d4e8d920bf1
simon911252 <simon911252@gmail.com>
2025-04-11 11:39:48 -0400

    Rajout des dimensions Garlando

--- projet_final/babyfoot_table_vpython.py
+++ projet_final/babyfoot_table_vpython.py
@@ -2,19 +2,22 @@ from vpython import canvas, box, sphere, vector, color, rate
 import numpy as np
 import math
 
-table_length = 20
-table_width = 10
-def_pos = [-table_width/5, table_width/5]
-mid_pos = [-2*table_width/5, -table_width/5, 0, table_width/5, 2*table_width/5]
-att_pos = [-3*table_width/10, 0, 3*table_width/10]
+#toutes les mesures sont en mm
 
-rod_positions = [-table_length/2+1, -table_length/3, -table_length/20, table_length/5]
-pwn_sz = [0.4, 0.8]
-ball_initial_pos, ball_initial_velocity, ball_radius = [0, 0], 4, 0.2
-ball_max_velocity, ball_min_velocity = 15, 2
+table_length, table_width = 1140, 700
+pwn_sz = [11, 21]
+sep_def, sep_mid, sep_att = 335, 122, 215  #separation head to head between players
+def_pos = [-sep_def/2, sep_def/2]
+mid_pos = [-2*sep_mid, -sep_mid, 0, sep_mid, 2*sep_mid]
+att_pos = [-sep_att, 0, sep_att]
 
-net_thickness, net_height, net_depth = 0.1, 3, 0.5
-rod_thickness = 0.1
+gk_rod_pos, def_rod_pos, mid_rod_pos, att_rod_pos = 45, 195, 498, 798 #position in mm from the gk end of the table
+rod_positions = [-table_length/2+gk_rod_pos, -table_length/2+def_rod_pos, -table_length/2+mid_rod_pos, -table_length/2+att_rod_pos]
+ball_initial_pos, ball_initial_velocity, ball_initial_dir = [0, 0], 50, [2, 0.5]
+ball_max_velocity, ball_min_velocity, ball_radius = 15, 2, 16
+
+net_thickness, net_height, net_depth = 0.1, 204, 40
+rod_thickness = 5
 
 # Simulation parameters
 dt = 0.02
@@ -28,39 +31,39 @@ table = box(pos=vector(0, 0, 0), size=vector(table_length, table_width, 0.1), co
 # Create the rods
 rods = []
 for pos in rod_positions:
-    rods.append(box(pos=vector(pos, 0, 0.15), size=vector(rod_thickness, table_width, rod_thickness), color=color.gray(0.5)))
+    rods.append(box(pos=vector(pos, 0, 0.15), size=vector(rod_thickness, table_width, 0.1), color=color.gray(0.5)))
 for pos in [-p for p in rod_positions]:
-    rods.append(box(pos=vector(pos, 0, 0.15), size=vector(rod_thickness, table_width, rod_thickness), color=color.gray(0.5)))
-
+    rods.append(box(pos=vector(pos, 0, 0.15), size=vector(rod_thickness, table_width, 0.1), color=color.gray(0.5)))
 
 # Create the players
 players = []
 
 # goalkeepers
-players.append(box(pos=vector(rod_positions[0], 0, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.blue)) #blue
-players.append(box(pos=vector(-rod_positions[0], 0, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.red)) #red
+players.append(box(pos=vector(rod_positions[0], 0, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.blue)) #blue
+players.append(box(pos=vector(-rod_positions[0], 0, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.red)) #red
 
 # defenders
 for y in def_pos:
-    players.append(box(pos=vector(rod_positions[1], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.blue)) #blue
+    players.append(box(pos=vector(rod_positions[1], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.blue)) #blue
 for y in def_pos:
-    players.append(box(pos=vector(-rod_positions[1], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.red)) #red
+    players.append(box(pos=vector(-rod_positions[1], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.red)) #red
 
 # midfielders
 for y in mid_pos:
-    players.append(box(pos=vector(rod_positions[2], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.blue)) #blue
+    players.append(box(pos=vector(rod_positions[2], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.blue)) #blue
 for y in mid_pos:
-    players.append(box(pos=vector(-rod_positions[2], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.red)) #red
+    players.append(box(pos=vector(-rod_positions[2], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.red)) #red
 
 # attackers
 for y in att_pos:
-    players.append(box(pos=vector(rod_positions[3], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.blue)) #blue
+    players.append(box(pos=vector(rod_positions[3], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.blue)) #blue
 for y in att_pos:
-    players.append(box(pos=vector(-rod_positions[3], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 0.1), color=color.red)) #red
+    players.append(box(pos=vector(-rod_positions[3], y, 0.15), size=vector(pwn_sz[0], pwn_sz[1], 1), color=color.red)) #red
 
 # Create the ball at the specified position
 ball = sphere(pos=vector(ball_initial_pos[0], ball_initial_pos[1], 0.15), radius=ball_radius, color=color.white)
-ball_velocity = vector(2*ball_initial_velocity, -0.5*ball_initial_velocity, 0)
+ball_initial_dir = [element / math.sqrt(ball_initial_dir[0]**2 + ball_initial_dir[1]**2) for element in ball_initial_dir]
+ball_velocity = vector(ball_initial_dir[0]*ball_initial_velocity, ball_initial_dir[1]*ball_initial_velocity, 0)
 
 
 # Create the nets behind each goalkeeper
@@ -75,6 +78,13 @@ def check_window_closed():
     global running
     if scene.visible == False:
         running = False
+
+def move_rod(color : str, rod_number : int, mov_amount : int):
+    #for rod number, gk = 0, def = 1, mid = 2, att = 3
+    #mov_amount in mm
+    pass
+
+
 
 while running:
     rate(50)
